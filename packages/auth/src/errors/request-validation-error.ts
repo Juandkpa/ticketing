c63630ce82decc719da7b39fa170feaaccd8ad40
Juan Parra <juandkpa@gmail.com>
2020-06-25 12:24:52 -0500

    auth error handling

---
+++ packages/auth/src/errors/request-validation-error.ts
@@ -0,0 +1,15 @@
+import { ValidationError } from 'express-validator';
+import { CustomError } from './custom-error';
+
+export class RequestValidationError extends CustomError {
+    statusCode = 400;
+
+    constructor(public errors: ValidationError[]) {
+        super('Request validation error');
+        Object.setPrototypeOf(this, RequestValidationError.prototype);
+    }
+
+    serializeErrors() {
+        return this.errors.map(err => ({ message: err.msg, field: err.param }));
+    }
+}
