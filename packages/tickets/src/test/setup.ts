0173f954563e1c179aa1f2079f7d95247611b5d0
Juan Parra <juandkpa@gmail.com>
2020-07-13 17:03:07 -0500

    ticketing microservice all routes

--- packages/tickets/src/test/setup.ts
+++ packages/tickets/src/test/setup.ts
@@ -39,7 +39,7 @@ afterAll(async () => {
 
 global.signin = () => {
     const payload = {
-        id: '188181',
+        id: new mongoose.Types.ObjectId().toHexString(),
         email: 'test@test.com'
     };
 
