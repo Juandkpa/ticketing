452d83d02708afc48f741f81c20d93f3697c5cca
Juan Parra <juandkpa@gmail.com>
2020-07-23 18:05:47 -0500

    implement version mogoose plugin

---
+++ packages/tickets/src/models/__test__/ticket.test.ts
@@ -0,0 +1,36 @@
+import { Ticket } from '../ticket';
+
+it('implements optimistic concurrency control', async () => {
+   const ticket = Ticket.build({
+       title: 'concert',
+       price: 5,
+       userId: '123'
+   });
+
+   await ticket.save();
+
+   const firstIntance = await Ticket.findById(ticket.id);
+   const secondInstance = await Ticket.findById(ticket.id);
+
+   firstIntance!.set({ price: 10 });
+   secondInstance!.set({ price: 15});
+
+   await firstIntance!.save();
+   await expect(secondInstance!.save()).rejects.toThrow();
+});
+
+it('increments the version number on multiple saves', async () => {
+    const ticket = Ticket.build({
+        title: 'concert',
+        price: 20,
+        userId: '123'
+    });
+
+    await ticket.save();
+    expect(ticket.version).toEqual(0);
+    await ticket.save();
+    expect(ticket.version).toEqual(1);
+    await ticket.save();
+    expect(ticket.version).toEqual(2);
+
+});;
